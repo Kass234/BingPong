d6d5c7dc5e1487a0c7efc7fd5b189e3176cc004c
Kaseem Thomas <60268531+Kass234@users.noreply.github.com>
2025-09-03 18:35:41 -0400

    button.py  attribute/method

--- SRC/button.py
+++ SRC/button.py
@@ -1,1 +1,13 @@
 
+import pygame
+
+class Button(pygame.sprite.Sprite):
+
+    def __init__(self, image, pos, width, height):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = pygame.image.load(image)
+        self.rect = pygame.Rect(pos[0], pos[1], width, height)
+        self.pos = pos
+        self.width = width
+        self.height = height
